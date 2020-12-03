85f021935a85879ede8223fdc1b6c1e5f3410461
ナモすけ <dynamorollerbot@gmail.com>
2020-12-03 15:28:31 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './main.scss'
+import homeIcon from './img/home.svg'
+import bellIcon from './img/bell.svg'
+import messageIcon from './img/message.svg'
+import profileIcon from './img/profile.svg'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Header />
+      <Contents />
+      <Footer />
+    </Router>
+  );
+}
+
+function Header() {
+  return <div className="header">
+    <Switch>
+      <Route path="/notifications">
+        <Notifications />
+      </Route>
+      <Route path="/messages">
+        <Messages />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/">
+        <span>Tegak</span>
+      </Route>
+    </Switch>
+  </div>;
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Link to="/"><img src={homeIcon} width="30" /></Link>
+      <Link to="/notifications"><img src={bellIcon} width="30" /></Link>
+      <Link to="/messages"><img src={messageIcon} width="30" /></Link>
+      <Link to="/profile"><img src={profileIcon} width="30" /></Link>
     </div>
   );
 }
 
-export default App;
+function Contents() {
+  return (
+    <div className="contents">
+      <Switch>
+        <Route path="/notifications">
+          <Notifications />
+        </Route>
+        <Route path="/messages">
+          <Messages />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Timeline />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Notifications() {
+  return (
+    <div>
+      通知
+    </div>
+  );
+}
+
+function Messages() {
+  return (
+    <div>
+      ダイレクトメッセージ
+    </div>
+  );
+}
+function Profile() {
+  return (
+    <div>
+      プロフィール
+    </div>
+  );
+}
+function Timeline() {
+  return (
+    <div>
+      タイムライン
+    </div>
+  );
+}
