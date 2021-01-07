9e16f9d1c940a512cedb3e026b813c8de0678d35
ナモすけ <dynamorollerbot@gmail.com>
2021-01-07 23:46:44 +0900

    SASS→Tailwind CSS置き換え等
    手書きCanvas用のクラスを通知コンポーネントにくっつけている、開発途中のため注意

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
 }
 
 function Header() {
-  return <div className="header">
+  return <div className="header sticky top-0 w-full text-center text-2xl pt-1 h-11">
     <Switch>
       <Route path="/notifications">通知</Route>
       <Route path="/messages">メッセージ</Route>
@@ -38,17 +38,17 @@ function Header() {
 function Footer() {
   return (
     <div className="footer">
-      <Link to="/"><img src={homeIcon} width="30" /></Link>
-      <Link to="/notifications"><img src={bellIcon} width="30" /></Link>
-      <Link to="/messages"><img src={messageIcon} width="30" /></Link>
-      <Link to="/profile"><img src={profileIcon} width="30" /></Link>
+      <Link to="/"><img src={homeIcon} width="30" alt="タイムライン" /></Link>
+      <Link to="/notifications"><img src={bellIcon} width="30" alt="通知" /></Link>
+      <Link to="/messages"><img src={messageIcon} width="30" alt="メッセージ" /></Link>
+      <Link to="/profile"><img src={profileIcon} width="30" alt="プロフィール" /></Link>
     </div>
   );
 }
 
 function Contents() {
   return (
-    <div className="contents">
+    <div className="m-3">
       <Switch>
         <Route path="/notifications">
           <Notifications />
@@ -68,7 +68,44 @@ function Contents() {
 }
 
 function Notifications() {
-  return <>通知</>;
+  useEffect(() => {
+    new TegakiCanvas();
+  });
+  return <canvas id="tegaki-canvas" className="bg-white"></canvas>;
+}
+
+class TegakiCanvas {
+  constructor() {
+    this.canvas = document.querySelector('#tegaki-canvas');
+    this.canvas.width = 400;
+    this.canvas.height = 400;
+    this.canvasPixelTimes = this.canvas.width / this.canvas.clientWidth;
+    this.ctx = this.canvas.getContext('2d');
+    this.cellX = 32;
+    this.cellY = 32;
+    this.cellArr = Array.from(new Array(this.cellY), () => new Array(this.cellX).fill(0));
+    this.paperRect = [10.5, 10.5, 380, 380];
+    this.ctx.fillStyle = 'black';
+    //this.ctx.strokeRect(...this.paperRect);
+
+
+    this.canvas.addEventListener('mousedown', e => {
+      this.mouseX = e.offsetX * this.canvasPixelTimes;
+      this.mouseY = e.offsetY * this.canvasPixelTimes;
+      this.cellArr[Math.floor(this.mouseY * 32 / 400)][Math.floor(this.mouseX * 32 / 400)] = 1;
+      this.draw();
+    });
+  }
+
+  draw() {
+    for (let y = 0; y < this.cellY; y++) {
+      for (let x = 0; x < this.cellX; x++) {
+        if (this.cellArr[y][x]) {
+          this.ctx.fillRect(Math.floor((400 / 32) * x), Math.floor((400 / 32) * y), Math.floor(400 / 32), Math.floor(400 / 32));
+        }
+      }
+    }
+  }
 }
 
 function Messages() {
