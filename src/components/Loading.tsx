89e20bca308ee86552df2a138a78330228e7153a
LuckyWind_sck <luckywindsck@gmail.com>
2021-04-25 13:15:22 +0900

    fix: add type declaration for Props

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import loadingIcon from '../assets/loading.svg';
 
-const Loading = (props: any) => <img {...props} src={loadingIcon} alt="読込中" />;
+interface Props {
+  className: string;
+}
+
+const Loading = (props: Props) => <img {...props} src={loadingIcon} alt="読込中" />;
 
 export default Loading;
