6438a11b04a30b2f60ebba2a8914dd31002b946b
rikako <riro.best.hum3@icloud.com>
2021-05-21 11:39:56 +0900

    アイコンの変更とヘッダーの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import profileIcon from '../assets/profile.svg';
+import serchIcon from '../assets/serch.svg';
 
 const Header = () => (
   <div className="header sticky top-0 w-full text-center text-2xl pt-1 h-11">
+    <Link to="/profile"><img src={profileIcon} width="30" alt="プロフィール" /></Link>
     <Route exact path="/">
-      <Link to="/">Tegaki</Link>
+      <Link to="/">Mojy</Link>
     </Route>
     <Route path="/notifications">
       <Link to="/notifications">通知</Link>
@@ -15,6 +18,7 @@ const Header = () => (
     <Route path="/profile">
       <Link to="/profile">プロフィール</Link>
     </Route>
+    <img src={serchIcon} width="30" alt="検索" />
   </div>
 );
 export default Header;
