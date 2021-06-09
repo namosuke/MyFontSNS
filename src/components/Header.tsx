136e596e3e9964952aa11741033b6e9c34550cf2
rikako <riro.best.hum3@icloud.com>
2021-06-09 14:00:15 +0900

    タブの追加とフォントセットに飛べるようにしました

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => (
       <Link to="/messages">メッセージ</Link>
     </Route>
     <Route path="/profile">
-      <Link to="/profile">プロフィール</Link>
+      <Link to="/profile">Mojy</Link>
     </Route>
     <img src={serchIcon} width="30" alt="検索" />
   </div>
--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,11 +1,86 @@
 import React from 'react';
-import { useCurrentUser, usePosts } from '../util/fetch';
+import { Link } from 'react-router-dom';
 
+import
+
+{
+  makeStyles,
+  Theme,
+  createMuiTheme,
+  ThemeProvider,
+}
+  from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { teal } from '@material-ui/core/colors';
+import { useCurrentUser, usePosts } from '../util/fetch';
 import Loading from '../components/Loading';
+
+interface TabPanelProps {
+  children: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const {
+    children, value, index, ...other
+  } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[500],
+    },
+    secondary: {
+      main: '#FDFDEA',
+    },
+  },
+});
 
 const Profile = () => {
   const currentUser = useCurrentUser();
   const posts = usePosts();
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
   if (!currentUser || !posts) return <Loading className="load-icon" />;
 
@@ -48,46 +123,80 @@ const Profile = () => {
         </div>
 
         <div>
-          <p className={[
-            'w-full',
-            'flex', 'items-center', 'justify-center',
-            'px-8', 'py-3',
-            'border', 'border-transparent',
-            'text-base', 'font-medium', 'text-white',
-            'rounded-md',
-            'bg-indigo-600', 'hover:bg-indigo-700',
-            'md:py-4', 'md:text-lg', 'md:px-10',
-          ].join(' ')}
-          >
-            フォントを見る
-          </p>
+          <Link to="/font/set">
+            <p className={[
+              'w-full',
+              'flex', 'items-center', 'justify-center',
+              'px-8', 'py-3',
+              'border', 'border-transparent',
+              'text-base', 'font-medium', 'text-white',
+              'rounded-md',
+              'bg-indigo-600', 'hover:bg-indigo-700',
+              'md:py-4', 'md:text-lg', 'md:px-10',
+            ].join(' ')}
+            >
+              フォントを見る
+            </p>
+          </Link>
         </div>
+        <ThemeProvider theme={theme}>
+          <div className={classes.root}>
+            <AppBar position="static" color="secondary">
+              <Tabs
+                value={value}
+                TabIndicatorProps={{
+                  style: {
+                    backgroundColor: '#D98032',
+                  },
+                }}
+                onChange={handleChange}
+                aria-label="simple tabs example"
+              >
+                <Tab label="ポスト" {...a11yProps(0)} />
+                <Tab label="返信" {...a11yProps(1)} />
+                <Tab label="メディア" {...a11yProps(2)} />
+                <Tab label="いいね" {...a11yProps(3)} />
+              </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+              <div className="posts-container">
+                {
+                  posts.filter((post) => post.user.id === currentUser.id)
+                    .map((post) => (
+                      <div
+                        key={post.id}
+                        className="ml-3 inline-flex rounded-md box-container"
+                      >
+                        <div className={[
+                          'items-center', 'justify-center',
+                          'px-5', 'py-3',
+                          'border', 'border-transparent',
+                          'text-base', 'font-medium',
+                          'rounded-md',
+                          'text-indigo-600', 'hover:bg-indigo-50i',
+                          'post-card',
+                        ].join(' ')}
+                        >
+                          <p className="text-lg name-tag">{post.user.name}</p>
+                          <p className="px-5 py-3">{post.text}</p>
+                        </div>
+                      </div>
+                    ))
+                }
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              返信
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              写真
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              いいね
+            </TabPanel>
+          </div>
+        </ThemeProvider>
 
-        <div className="posts-container">
-          {
-            posts.filter((post) => post.user.id === currentUser.id)
-              .map((post) => (
-                <div
-                  key={post.id}
-                  className="ml-3 inline-flex rounded-md box-container"
-                >
-                  <div className={[
-                    'items-center', 'justify-center',
-                    'px-5', 'py-3',
-                    'border', 'border-transparent',
-                    'text-base', 'font-medium',
-                    'rounded-md',
-                    'text-indigo-600', 'hover:bg-indigo-50i',
-                    'post-card',
-                  ].join(' ')}
-                  >
-                    <p className="text-lg name-tag">{post.user.name}</p>
-                    <p className="px-5 py-3">{post.text}</p>
-                  </div>
-                </div>
-              ))
-          }
-        </div>
       </div>
     </>
   );
