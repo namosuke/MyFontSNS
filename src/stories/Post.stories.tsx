51ce79858e64e9fd031c184c5db1f7b1be03e275
ナモすけ <dynamorollerbot@gmail.com>
2021-06-28 03:40:25 +0900

    PostコンポーネントをStorybookに追加

---
+++ src/stories/Post.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import Post, { PostProps } from '../components/Post';
+
+export default {
+  title: 'MyFontSNS/Post',
+  component: Post,
+  parameters: {
+    backgrounds: {
+      default: 'lemonchiffon',
+      values: [
+        { name: 'lemonchiffon', value: 'lemonchiffon' },
+      ],
+    },
+  },
+} as Meta;
+
+const Template: Story<PostProps> = (args) => <Post {...args} />;
+
+export const Test1 = Template.bind({});
+Test1.args = {
+  post: {
+    id: 2000,
+    text: 'おはようございます！！\n今日も一日頑張りましょう！！\n#朝の挨拶 https://ohayo.net/',
+    user: {
+      name: 'おはようbot',
+    },
+  },
+};
