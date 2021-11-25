d14fe0f15b8ce0c2c6e3a94b8dac5e0ba1fdccf4
ナモすけ <dynamorollerbot@gmail.com>
2021-11-26 05:12:51 +0900

    added postCanvas
    タイムラインと投稿画面でそれぞれ表示できる
    参照すべき文字が無かったときのフォールバックを提供している
    
    要素の大きさを反映できておらず、100×100pxになりがち（要素の大きさが変わったら通知するようなフックを導入すべき？）
    なぜかタイムラインで文字の最初に謎のインデントが付く

--- src/components/postCanvas.tsx
+++ src/components/postCanvas.tsx
@@ -4,57 +4,78 @@
 /* eslint-disable no-param-reassign */
 // https://nimil.jp/blog/p5js-react
 import p5 from "p5";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import axios from "axios";
 
-let postCanvasText: string;
+const PostCanvas = ({ text }: { text: string }) => {
+  const postCanvasText = useRef(text);
+  const p5Ref = useRef<null | p5>(null);
+  const postCanvasElement = useRef<HTMLDivElement>(null);
+  postCanvasText.current = text;
+  if (p5Ref.current) {
+    p5Ref.current.redraw();
+  }
 
-const sketch = async (p: p5) => {
-  const { data: fontFile } = await axios.get("./seeds/mfsFont.json");
+  const sketch = async (p: p5) => {
+    const { data: fontFile } = await axios.get("./seeds/mfsFont.json");
+    const scaling = 0.5;
 
-  p.setup = () => {
-    // Ctrl + F5じゃないと反映しません
-    p.createCanvas(p.windowWidth, p.windowHeight).parent("post-canvas");
-  };
+    p.setup = () => {
+      if (postCanvasElement.current) {
+        // Ctrl + F5じゃないと反映しません
+        p.createCanvas(postCanvasElement.current.clientWidth, 200);
+      }
+      p.noLoop();
+      p5Ref.current = p;
+    };
 
-  p.draw = () => {
-    p.clear();
-    const spreadPostCanvasText = postCanvasText.split("");
-    const currentBasePosition = { x: 0, y: 0 };
-    spreadPostCanvasText.forEach((char) => {
-      if (char in fontFile?.data) {
-        const height = fontFile.data[char].length;
-        const width = fontFile.data[char][0].length;
-        if (currentBasePosition.x + width > p.windowWidth) {
-          currentBasePosition.y += fontFile.defaultHeight + 20;
-          currentBasePosition.x = 0;
-        }
-        for (let h = 0; h < height; h += 1) {
-          for (let w = 0; w < width; w += 1) {
-            if (fontFile.data[char][h][w] === 1) {
-              p.point(currentBasePosition.x + w, currentBasePosition.y + h);
+    p.draw = () => {
+      p.clear();
+      p.scale(scaling);
+
+      const spreadPostCanvasText = postCanvasText.current.split("");
+      const currentBasePosition = { x: 0, y: 0 };
+      spreadPostCanvasText.forEach((char) => {
+        if (char in fontFile?.data) {
+          const height = fontFile.data[char].length;
+          const width = fontFile.data[char][0].length;
+          if (currentBasePosition.x + width > p.width / scaling) {
+            currentBasePosition.y += fontFile.defaultHeight + 20;
+            currentBasePosition.x = 0;
+          }
+          for (let h = 0; h < height; h += 1) {
+            for (let w = 0; w < width; w += 1) {
+              if (fontFile.data[char][h][w] === 1) {
+                p.point(currentBasePosition.x + w, currentBasePosition.y + h);
+              }
             }
           }
+          currentBasePosition.x += width;
+        } else if (char === "\n") {
+          currentBasePosition.y += fontFile.defaultHeight + 20;
+          currentBasePosition.x = 0;
+        } else {
+          if (currentBasePosition.x + p.textWidth(char) > p.width / scaling) {
+            currentBasePosition.y += fontFile.defaultHeight + 20;
+            currentBasePosition.x = 0;
+          }
+          p.textSize(fontFile.defaultHeight);
+          p.text(
+            char,
+            currentBasePosition.x,
+            currentBasePosition.y + fontFile.defaultHeight - 5
+          );
+          currentBasePosition.x += p.textWidth(char);
         }
-        currentBasePosition.x += width;
-      } else if (char === "\n") {
-        currentBasePosition.y += fontFile.defaultHeight + 20;
-        currentBasePosition.x = 0;
-      } else {
-        console.log("data not found");
-      }
-    });
+      });
+    };
   };
-};
-
-const PostCanvas = ({ text }: { text: string }) => {
-  postCanvasText = text;
 
   useEffect(() => {
-    new p5(sketch);
+    new p5(sketch, postCanvasElement.current ?? undefined);
   }, []);
 
-  return <div id="post-canvas" />;
+  return <div ref={postCanvasElement} />;
 };
 
 export default PostCanvas;
--- src/views/Createpost.tsx
+++ src/views/Createpost.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
-import { useFont } from '../util/fetch';
-import PostCanvas from '../components/postCanvas';
+import React, { useState } from "react";
+import { useFont } from "../util/fetch";
+import PostCanvas from "../components/postCanvas";
 
 /* const Createpost = () => {
   const font = useFont();
@@ -24,17 +24,21 @@ import PostCanvas from '../components/postCanvas';
 */
 
 const Createpost = () => {
-  const [textarea, setTextarea] = useState('');
-  const font = useFont();
+  const [textarea, setTextarea] = useState("");
+  /* const font = useFont();
   if (font) {
     for (let i = 0; i < textarea.length; i += 1) {
       console.log(font.data[textarea[i]]);
     }
-  }
+  } */
   return (
     <>
-      <textarea value={textarea} onChange={(e) => setTextarea(e.target.value)} className="m-4 text-base p-1 border border-gray-400 w-80" />
-      <PostCanvas text={textarea}/>
+      <textarea
+        value={textarea}
+        onChange={(e) => setTextarea(e.target.value)}
+        className="m-4 text-base p-1 border border-gray-400 w-80"
+      />
+      <PostCanvas text={textarea} />
     </>
   );
 };
