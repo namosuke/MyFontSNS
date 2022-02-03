356e9ea71563b39602fa4d9b2bc0e3b6d0ab623a
ナモすけ <dynamorollerbot@gmail.com>
2022-02-03 14:54:36 +0900

    component returns post data

--- src/views/Createpost.tsx
+++ src/views/Createpost.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable max-len */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "../components/Post";
 import { useFont } from "../util/fetch";
 
 const Createpost = () => {
   const [textarea, setTextarea] = useState("");
+  const [postName, setPostName] = useState("");
   const font = useFont();
   const post = () => {
     if (font) {
@@ -21,14 +22,31 @@ const Createpost = () => {
       console.log(postData);
     }
   };
+  useEffect(() => {
+    console.log({
+      user: {
+        id: 0,
+        name: postName,
+        screen_id: "dummy",
+      },
+      id: 0,
+      text: textarea,
+    });
+  }, [textarea, postName]);
   return (
     <>
+      <input
+        type="text"
+        value={postName}
+        onChange={(e) => setPostName(e.target.value)}
+        className="block mx-auto mt-4 p-1 border border-gray-400 w-96 text-lg"
+      />
       <textarea
         value={textarea}
         onChange={(e) => setTextarea(e.target.value)}
-        className="block mx-auto mt-4 text-base p-1 border border-gray-400 w-96 text-lg"
+        className="block mx-auto mt-4 p-1 border border-gray-400 w-96 text-lg"
       />
-      <Post post={{ user: { name: "プレビュー" }, text: textarea }} />
+      <Post post={{ user: { name: postName }, text: textarea }} />
       <button
         type="button"
         className="block w-max h-max bg-yellow-600 rounded-full text-white text-xl px-[10px] mt-6 mx-auto"
